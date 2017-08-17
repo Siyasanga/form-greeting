8c7ec3c35da7b65438e93ff4906d94a2f6d08a61
Siyasanga <siyasanga@live.co.za>
2017-08-17 16:28:40 +0200

    Fix generateMsg and captureName functions

--- app.js
+++ app.js
@@ -12,40 +12,33 @@ app.engine("hbs", exhbs({ defaultLayout:"main",
                           extname:"hbs"}));
 app.set("view engine","hbs");
 //***********************Capture Name**************************
-function captureName(inName) {
+function captureName(inName,lang,res) {
   user.findOne({name:inName}, function(err, doc) {
-    if(doc){
+    if(doc){  // username already exists
       user.update({name:inName}, {$inc: {greetings:1}}, function(err, affected) {
-        if(affected)
+        if(affected) // confirm update
           console.log("Greetings for "+inName+" successfully updated.");
+          user.count(function(err, result) {
+              console.log("Count result after updating"+result);
+              res.render("form",{msg:generateMsg(inName,lang),count:result});
+          });
       });
     } // end updating greetings for the user
-    else {
+    else {  // new user needs to be recorded
       var newUser = new user({name:inName, greetings:1});
       newUser.save(function(err, doc) {
       if(err)
         console.log(err);
       else
         console.log("successfully registered!\n"+doc);
+        user.count(function(err, result) {
+          console.log("Count results: "+result);
+          res.render("form",{msg:generateMsg(inName,lang),count:result});
+        });
       });
     } // end registering new user
   }); // end of findOne
 } // end captureName
-//*************************************************************
-// captureName("Anele");
-//*********************Format table Data***********************
-function formatTbl(){
-  output = "";
-  var greeted;
-  user.find(function(err, db) {
-    greeted = db;
-    for(var i in greeted){
-      output = output+"<tr><td class='names'>"+greeted[i].name+"</td><td>"+greeted[i].greetings+"</td></tr>";
-    }
-    return output;
-  });
-} // end of formatTbl
-console.log(formatTbl());
 //*****************Generating the greeting message*********************
 function generateMsg(name,lang) {
   var htmlMsg;
@@ -62,14 +55,26 @@ function generateMsg(name,lang) {
   }
   return htmlMsg;
 }
+//*************************************************************
+//*********************Format table Data***********************
+function formatTbl(res){
+  output = "";
+  user.find(function(err, greeted) {
+    for(var i in greeted){
+      output = output+"<tr><td class='names'>"+greeted[i].name+"</td><td>"+greeted[i].greetings+"</td></tr>";
+    }
+    res.render("greeted",{tbody:output});
+  });
+} // end of formatTbl
 //********************************************************************
-// app.get("/",function(req,res) {
-//   res.render("form",{count:userCounter()+1});
-// });
-// app.get("/greeted", function(req,res) {
-//   formatTbl();
-//   res.render("greeted",{tbody:formatTbl()});
-// });
+app.get("/",function(req,res) {
+  user.count(function(err, counter) {
+    res.render("form",{count:counter});
+  });
+});
+app.get("/greeted", function(req,res) {
+  formatTbl(res);
+});
 // app.get("/deleteAll",function (req,res) {
 //   greeted = {};
 //   updateGreetBackup();
@@ -78,20 +83,18 @@ function generateMsg(name,lang) {
 // app.post("/deleteAll",function (req,res) {
 //   res.redirect("/");
 // });
-// app.post('/', function(req,res) {
-//   igama = req.body.name;
-//   language = req.body.lang;
-//   if(igama.length > 0){
-//     if(language){
-//       console.log(igama+" Accepted!");
-//       captureName(igama);
-//       res.render("form",{msg:generateMsg(igama,language),count:userCounter()});
-//     }
-//   }
-// });
-// //***Start Server***
-// // var hostname = "127.0.0.1";
-// var port = process.env.PORT || 3000;
-// app.listen(port, function() {
-//   console.log("Server running at http://localhost:"+port+"/");
-// });
+app.post('/', function(req,res) {
+    igama = req.body.name;
+    language = req.body.lang;
+    if(igama.length > 0){
+      if(language){
+        console.log(igama+" Accepted!");
+        captureName(igama,language,res);
+      }
+    }
+});
+// ***Start Server***
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Server running at http://localhost:"+port+"/");
+});
