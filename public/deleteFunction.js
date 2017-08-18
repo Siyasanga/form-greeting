1d8e93bcb3aa50a64f65c86d4a80c84a36d72e48
Siyasanga <siyasanga@live.co.za>
2017-08-18 12:10:20 +0200

    Make redirect to root directly after delete animation

--- public/deleteFunction.js
+++ public/deleteFunction.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
   console.log("we back");
   setTimeout(function() {
     document.querySelector('#delConfirm').style.display = "none";
+    window.location = "http://localhost:3000/";
   },2500);
 })
